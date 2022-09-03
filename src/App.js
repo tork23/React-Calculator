325b7cc6f8c9afe0d73a35643209ae3eeee6ba4f
tork23 <prajwaltorkadi@gmail.com>
2022-09-03 16:54:42 +0530

    update style

--- src/App.js
+++ src/App.js
@@ -30,64 +30,65 @@ function App() {
 
   return (
     <div className="calculator-app">
-      {/* Display */}
+      {/* Result Display */}
+      <h1>Calculator</h1>
       <form>
         <input type="text" value={result} ref={inputRef} />
       </form>
       {/* Keypad */}
       <div className="keypad">
-        <button id="clear" onClick={clear}>
+        <button id="clear" className="grid-item" onClick={clear}>
           Clear
         </button>
-        <button id="backspace" onClick={backspace}>
+        <button id="backspace" className="grid-item" onClick={backspace}>
           Back
         </button>
-        <button name="+" onClick={handleClick}>
+        <button name="+" className="grid-item" onClick={handleClick}>
           +
         </button>
-        <button name="7" onClick={handleClick}>
+        <button name="7" className="grid-item" onClick={handleClick}>
           7
         </button>
-        <button name="8" onClick={handleClick}>
+        <button name="8" className="grid-item" onClick={handleClick}>
           8
         </button>
-        <button name="9" onClick={handleClick}>
+        <button name="9" className="grid-item" onClick={handleClick}>
           9
         </button>
-        <button name="-" onClick={handleClick}>
+        <button name="-" className="grid-item" onClick={handleClick}>
           -
         </button>
-        <button name="4" onClick={handleClick}>
+        <button name="4" className="grid-item" onClick={handleClick}>
           4
         </button>
-        <button name="5" onClick={handleClick}>
+        <button name="5" className="grid-item" onClick={handleClick}>
           5
         </button>
-        <button name="6" onClick={handleClick}>
+        <button name="6" className="grid-item" onClick={handleClick}>
           6
         </button>
-        <button name="*" onClick={handleClick}>
+        <button name="*" className="grid-item" onClick={handleClick}>
           *
         </button>
-        <button name="1" onClick={handleClick}>
+        <button name="1" className="grid-item" onClick={handleClick}>
           1
         </button>
-        <button name="2" onClick={handleClick}>
+        <button name="2" className="grid-item" onClick={handleClick}>
           2
         </button>
-        <button name="3" onClick={handleClick}>
+        <button name="3" className="grid-item" onClick={handleClick}>
           3
         </button>
-        <button name="/" onClick={handleClick}>
+        <button name="/" className="grid-item" onClick={handleClick}>
           /
         </button>
-        <button name="0" onClick={handleClick}>
+        <button name="0" className="grid-item" onClick={handleClick}>
           0
         </button>
-        <button name="." onClick={handleClick}>
+        <button name="." className="grid-item" onClick={handleClick}>
           .
         </button>
-        <button id="result" onClick={calculate}>
+        <button id="result" className="grid-item" onClick={calculate}>
           =
         </button>
       </div>
